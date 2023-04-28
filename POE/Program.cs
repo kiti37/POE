ee3841f8373aced662ea8e773d51f9aa4ed7bf3c
kiti37 <101188177+kiti37@users.noreply.github.com>
2023-04-28 17:02:24 +0200

    Add files via upload

---
+++ POE/Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+using POE;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //----------------------------------------Start---------------------------------------//
+        //Makes the worker class an object
+        Worker recipe = new Worker();
+
+        //While loop for creating a menu system that accepts user input
+        while (true)
+        {
+            Console.WriteLine("Enter 1 to add recipe details");//Asks user input
+            Console.WriteLine("Enter 2 to view recipe");//Asks user input
+            Console.WriteLine("Enter 3 to scale the  recipe");//Asks user input
+            Console.WriteLine("Enter 4 to reset the recipe quantity");//Asks user input
+            Console.WriteLine("Enter 5 to delete the recipe");//Asks user input
+            Console.WriteLine("Enter 6 to exit");//Asks user input
+
+            //Stores user input
+            string number = Console.ReadLine();
+
+            //Switch and case for calling methods
+            switch (number)
+            {
+                case "1":
+                    recipe.Details();
+                    break;
+                case "2":
+                    recipe.DetailDisplay();
+                    break;
+                case "3":
+                    recipe.Scaling();
+                    break;
+                case "4":
+                    recipe.ResetQuantities(); 
+                    break;
+                case "5":
+                    recipe.ResetQuantities();
+                    break;
+                    case "6":
+                    recipe.Exit();
+                    break;
+            }
+
+        }
+        //----------------------------------------End---------------------------------------//
+    }
+}
---
+++ POE/Worker.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POE
+{
+    //----------------------------------------Start---------------------------------------//
+    internal class Worker
+    {
+        //Declaration of Variables and arrays
+        int[] Number;
+        String[] Ingredient;
+        Double[] Quantity;
+        String[] Units;
+        int[] stepsNum;
+        String[] Steps;
+
+        //Method for commencing empty arrays for user input
+        public void Recipe()
+            {
+            Number = new int[0]; 
+            Ingredient = new String[0];
+            Quantity = new Double[0];
+            Units = new String[0];
+            Steps = new String[0];
+            }
+
+        //Method for asking user for input of recipe details
+        public void Details()
+        {
+            //Asks user to enter the number of ingredients
+            Console.WriteLine("Enter number of Ingredients: ");
+            int Number = int.Parse(Console.ReadLine());
+
+            //Apples the intended size of arrays
+            Ingredient = new string[Number];
+            Quantity = new double[Number];
+            Units = new string[Number];
+            stepsNum = new int[Number];
+            Steps = new string[Number]; 
+
+            //Asks user to enter the details of the recipe
+            for (int a = 0; a < Number; a++)
+            {
+                Console.WriteLine("Enter details for ingredient: ");
+
+
+                Console.WriteLine("Name :");
+                Ingredient[a] = Console.ReadLine();//Fills the array with user input
+                
+
+                Console.WriteLine("Quantity: ");
+                Quantity[a] = double.Parse(Console.ReadLine());//Fills the array with user input
+
+                Console.WriteLine("Enter the measurement Unit: ");
+                Units[a] = Console.ReadLine();
+
+                Console.WriteLine("Enter the number of steps: ");
+                stepsNum[a] = int.Parse(Console.ReadLine());//Fills the array with user input
+
+                Console.WriteLine("Steps: ");
+                Steps[a] = Console.ReadLine();//Fills the array with user input
+
+
+            }
+        }
+        //Method to view recipe details
+        public void DetailDisplay()
+        {
+            Console.WriteLine("Recipe Details: ");
+
+            for (int b = 0; b < Ingredient.Length; b++)
+            {
+                Console.WriteLine($"Ingredients: {Ingredient[b]}");
+            }
+            for (int c = 0; c < Quantity.Length; c++)
+            {
+                Console.WriteLine($"Quantity: {Quantity[c]}");
+            }
+            for (int d = 0; d < Units.Length; d++)
+            {
+                Console.WriteLine($"Units: {Units[d]}");
+            }
+            for (int e = 0; e < Steps.Length; e++)
+            {
+                Console.WriteLine($"Steps: {Steps[e]}");
+            }
+
+        }
+
+        //Method to scale quantity
+        public void Scaling()
+        {
+            Console.Write("Enter scale factor (0.5, 2, or 3): ");
+            double Scale = double.Parse(Console.ReadLine());
+
+            //for loop for scaling and filling array with the scale amount
+            for (int f = 0; f < Scale; f++)
+            {
+                Quantity[f] = Scale;
+            }
+            Console.WriteLine("Scaling has been succesfully scaled");
+        }
+        //Method for resetting Quantities
+        public void ResetQuantities()
+        {
+            // Resets the quantities
+            double newQuantity = Convert.ToDouble(Console.ReadLine());
+
+            for (int g = 0; g < Quantity.Length; g++)
+            {
+                Quantity[g] = newQuantity;
+            }
+            Console.WriteLine("Quantity has been reset successfully");
+        }
+        public void ClearData()
+        {
+            // Reset all the arrays to empty
+            Number = new int[0];
+            Ingredient = new String[0];
+            Quantity = new Double[0];
+            Units = new String[0];
+            Steps = new String[0];
+        }
+        //Method for exiting application
+        public void Exit()
+        {
+            System.Environment.Exit(0);
+        }
+    }//----------------------------------------End---------------------------------------//
+}
